02a63c1ffc51bf8d3a6d1d5ba06c6c4970ae2c26
Laynooo <alan20049339@gmail.com>
2026-05-06 15:41:58 +0800

    Add files via upload

---
+++ ex11/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HelloCGU from './cgu_hello';
+import MultiButton from './cgu_multiButton';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloCGU />
+      <MultiButton />
+    </div>
+  );
+}
+
+export default App;
